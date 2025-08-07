27ecc1646332c56f710a9e5ac14fd0260e2d9211
DevDaniel21 <danielglaucia2.00@gmail.com>
2025-08-07 17:36:04 -0300

    Configurações inciais do projeto

---
+++ src/components/Topo/index.js
@@ -0,0 +1,17 @@
+export default function Topo() {
+    return (
+        <header>
+            <div>
+                <picture>
+                    <a href=""><img src="./blackjack.svg"/></a>
+                </picture>
+
+                <nav>
+                    <a href="#produto">Produtos</a>
+                    <a href="#produto">Sobre</a>
+                    <a href="#produto">Produtos</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import estilos from "@/styles/Home.module.css";
+import Topo from "@/components/Topo"
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Topo />
     </>
   );
 }
